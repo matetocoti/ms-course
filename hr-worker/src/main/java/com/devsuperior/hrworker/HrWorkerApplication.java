db3c365aeb6305155d29c1875af77a5d9a2c93b8
Matheus Costa <matheuscosta_111@hotmail.com>
2024-02-15 02:36:53 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -11,6 +11,7 @@ public class HrWorkerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrWorkerApplication.class, args);
+		
 	}
 
 }
--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -5,7 +5,11 @@ package com.devsuperior.hrworker.resources;
 //--(IMPORTS)--//
 
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
 
+
 //--(END)--//
 
 
@@ -21,8 +26,28 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+	//O código cria um logger chamado logger para a classe WorkerResource, 
+	//permitindo que mensagens de log sejam registradas durante a execução do programa. 
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	//Guarda valor da configuração de teste
+	@Value("${test.config}")
+	private String testConfig;
+		
 	@Autowired
 	private WorkerRepository workerRepository;
+	
+
+	
+	//Método que pega as configurações do servidor de configurações
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs(){
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+	
 	
 	
 	@GetMapping
@@ -35,4 +60,6 @@ public class WorkerResource {
 		return ResponseEntity.ok().body(workerRepository.findById(id).get());
 	}
 	
+	
+
 }
