58e05f7cbce8a90999eca1c3140009f13e55cce2
Matheus Costa <matheuscosta_111@hotmail.com>
2024-03-07 18:02:38 -0300

    Configuring hr-oauth project

--- hr-oauth/src/main/java/com/devsuperior/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/HrOauthApplication.java
@@ -1,8 +1,12 @@
 package com.devsuperior.hroauth;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HrOauthApplication {
 
